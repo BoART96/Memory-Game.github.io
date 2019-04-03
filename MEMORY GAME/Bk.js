f90c510f8b0f27de4a946f6fa779b95e53daa064
Wonder13oy <wandilenxumalo131@gmail.com>
2019-04-03 11:04:46 +0200

    Removed the extra, not needed html files

--- MEMORY GAME/Bk.js
+++ MEMORY GAME/Bk.js
@@ -1,4 +1,3 @@
-const cards = document.querySelectorAll('.Card');
 
 let hasFlippedCard = false;
 let lockBoard = false; //To stop the user from picking more than 2 cards
@@ -9,6 +8,7 @@ function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;//prevents the user from double clicking on the same card
   this.classList.add('flip');
+  console.log(this);
 
  //Stops the user from flipping the same card twice
   if (!hasFlippedCard) {
@@ -56,29 +56,65 @@ function resetBoard() {
 }
 
 //To randomise the positions of the cards on the board  --> Learnt this syntax on ES6(Call right after creating function)
-(function shuffle() {
+function shuffle(cards) {
   cards.forEach(card => {
     let randomPos = Math.floor(Math.random() * 12);
     card.style.order = randomPos;
   });
-})();
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
 
 //_________________________________________This is where Wandile's Edits starts__________________________________________________
 
 function changeDifficulty() {
   const selector = document.getElementById('selector').value;
+  loadGame(selector);
 
-  if (selector == 2) {
-    location.assign('level1.html');
-  } else if (selector == 3) {
-    location.assign('level2.html');
-  } else if (selector == 4){
-    location.assign('level3.html');
-  } else if (selector == 5) {
-    location.assign('level4.html');
-  } else {
-    location.assign('level5.html');
+}
+
+function loadGame(selection) {
+  if (document.getElementById('anything').hasChildNodes()) {
+    document.getElementById("anything").removeChild(anything.childNodes[0]);
   }
+
+  const cardPack = ["img\\a.jpg.jpg", "img\\a.jpg.jpg", "img\\body.jpg.jpg",
+                  "img\\body.jpg.jpg", "img\\br.jpg.jpg", "img\\br.jpg.jpg",
+                  "img\\Class.jpg.jpg", "img\\Class.jpg.jpg", "img\\em.jpg.jpg",
+                  "img\\em.jpg.jpg", "img\\html.jpg.jpg", "img\\html.jpg.jpg"];
+
+  const dataSets = ["A", "A", "BODY", "BODY", "BREAK", "BREAK", "CLASS", "CLASS", "EM", "EM", "HTML", "HTML"];
+  const coverCard = "img\\ctrlZ.jpg.jpg";
+
+  let section = document.createElement('section');
+  section.classList.add('memory-game');
+  changeGrid(selection);
+  for (var i = 0; i < selection * 2; i++) {
+    console.log(i);
+
+    let divs = document.createElement("div");
+    divs.classList.add("Card");
+    divs.dataset.framework = dataSets[i];
+
+    let imageFront = document.createElement("img");
+    imageFront.classList.add("main");
+    imageFront.src = cardPack[i];
+    divs.appendChild(imageFront);
+
+    let imageBack = document.createElement("img");
+    imageBack.classList.add("cover");
+    imageBack.src = coverCard;
+
+    divs.appendChild(imageBack);
+    section.appendChild(divs);
+  }//For loop ends
+  document.getElementById('anything').appendChild(section);
+  console.log("This works");
+
+  const cards = document.querySelectorAll('.Card');
+  shuffle(cards);
+  cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+function changeGrid(columns) {
+  document.getElementById('anything').style.gridTemplateColumns = "1fr 1fr";
 }
